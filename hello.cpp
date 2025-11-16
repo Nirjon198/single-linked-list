d5e16382e79e8c6820ed8b55c3742370562d780c
Nirjon198 <mahdi7224@gmail.com>
2025-11-16 23:01:27 +0600

    ignore the name.this is about list and iterator

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <list>
+
 using namespace std;
 int main(){
-    cout<<"hello";
-    return 0;
+ list<string>name={"Nirjon","Shawon","Rifat","Rafi"};
+ list<string>::iterator itr;
+ for (itr = name.begin(); itr != name.end(); itr++) // iterators only support != (inequality).This asks, "Is my current list node the same as the end node?"
+ {
+    cout<<*itr<<endl;
+ }
+ return 0;
+
 }
