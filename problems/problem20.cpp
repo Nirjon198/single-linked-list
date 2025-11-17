916e435dde841e05babe88c364200323aa357e1b
Nirjon198 <mahdi7224@gmail.com>
2025-11-18 05:54:50 +0600

    appended

--- problems/problem20.cpp
+++ problems/problem20.cpp
@@ -30,20 +30,25 @@ class List{
             tail=newNode;
         }
     }
-    void appended(List &other){
-        if(head==NULL){
+    void appended(List &other) //(class_Name &local_name_of_list2) & means the function will not copy the object.it work on original list2 object
+    {
+        if (head == NULL) // ব্যাখ্যা: এই লাইনটি পরীক্ষা করে যে, বর্তমান লিস্টটি (অর্থাৎ যে লিস্টের জন্য এই ফাংশনটি কল করা হয়েছে) সম্পূর্ণ খালি কি না। (head হলো বর্তমান লিস্টের প্রথম নোড)।
+        {
             head=other.head;
-            tail=other.tail;
+            tail = other.tail; // head=other.head;: বর্তমান লিস্টের head এখন other লিস্টের head-কে পয়েন্ট করবে।
+
+            /*     tail = other.tail;: বর্তমান লিস্টের tail এখন other লিস্টের tail-কে পয়েন্ট করবে।
+
+             ফলাফল: বর্তমান খালি লিস্টটি কার্যকরভাবে other লিস্টে পরিণত হয়।*/
+                
         }
         else if(other.head !=NULL){
             tail->next=other.head;
             tail=other.tail;
-
-        }
-        else{
             other.head = NULL;
             other.tail = NULL;
         }
+
     }
     void temporary(){
         Node* temp=head;
