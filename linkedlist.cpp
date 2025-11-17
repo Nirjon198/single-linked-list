c03c881396a0f04222f5f53ffd2a7b12b8bdb64c
Nirjon198 <mahdi7224@gmail.com>
2025-11-17 17:16:47 +0600

    front_push

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -38,14 +38,35 @@ class List{
                                     //     তাই লিস্টটি সেই নোডটিকে হারাবে না।
              if (head == NULL)
         {
-            head=tail=NULL;
-            return ;
+            head=newNode;
+            tail=newNode;
+           
         }
         else{
             newNode->next = head;
+            head=newNode;
+            // "->" this arrow means: (*newNode).next=head which means the the new Node which is the newNode pointer,we will dereference it and assign to "head"
+            
         }
+    }
+    void printLL(){
+        Node* temp =head;
+        while(temp!=NULL){
+            cout<< temp->data<<" ";
+            temp=temp->next;
+
+        }
+        cout<<endl;
     }
 };
 int main(){
+    List ll;
+    ll.push_front(1);
+    ll.push_front(2);
+    ll.push_front(3);
+    ll.push_front(4);
+    ll.printLL();
+    return 0;
+
 
 }
